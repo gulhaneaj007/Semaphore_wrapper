6c785e0421ac1bb8a9cfcf99d25ad5be86d88f7d
L00187577 <L00187577@atu.ie>
2025-08-06 21:02:22 +0530

    portchange

--- backend/app.js
+++ backend/app.js
@@ -125,7 +125,7 @@ app.post('/api/proxmox_creds', async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`API listening on port ${port}`);
 });
